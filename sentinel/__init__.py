0eb1149c4b90dabdd63f35501b6ab693eb40e3ee
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2021-11-03 21:35:25 +0000

    Implement New API

--- sentinel/__init__.py
+++ sentinel/__init__.py
@@ -1,2 +1,34 @@
-from .base   import *
-from .common import *
+__all__ = ['SentinelMeta', 'Sentinel', 'Missing']
+
+class SentinelMeta(type):
+    """ Metaclass for Sentinel """
+
+    def __new__(metaclass, name, bases, namespace):
+        def __new__(cls, *args, **kwargs):
+            raise TypeError(f'type {cls.__name__!r} is not an acceptable base type')
+
+        def __setattr__(cls, name, value):
+            raise AttributeError(f'{cls.__name__!r} object has no attribute {name!r}')
+
+        def __getattr__(cls, name):
+            raise AttributeError(f'{cls.__name__!r} object has no attribute {name!r}')
+
+        namespace['__new__']     = __new__
+        namespace['__setattr__'] = __setattr__
+        namespace['__getattr__'] = __getattr__
+
+        cls = super().__new__(metaclass, name, bases, namespace)
+
+        super().__setattr__(cls, '__class__', cls)
+
+        return cls
+
+    def __repr__(cls):
+        return cls.__name__
+
+class Sentinel(SentinelMeta, metaclass=SentinelMeta): pass
+
+# Allow subclassing
+del Sentinel.__new__
+
+class MISSING(Sentinel): pass
--- sentinel/utils.py
+++
@@ -1,6 +0,0 @@
-import typing
-
-from . import base
-
-def is_sentinel(obj: typing.Any) -> bool:
-    return type(obj) is base.SentinelMeta
